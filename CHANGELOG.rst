892badd65ae14d24e19d817f67d98f7c92d664c0
Wesley Baugh <wesley@bwbaugh.com>
2015-11-12 23:41:07 -0800

    Prototype the basic functionality

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -9,3 +9,9 @@ This project adheres to `Semantic Versioning <http://semver.org/>`_.
 **********
 Unreleased
 **********
+
+Added
+=====
+
+* Minimum viable product (MVP) functionality.
+  Updates the screen with running counts.
--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,39 @@
+import Data.List (elemIndex, intersperse)
+import Data.Maybe (fromJust)
+import System.Console.ANSI (clearFromCursorToLineEnd, cursorUpLine)
+
 main :: IO ()
-main = return ()
+main = do
+    contents <- getContents
+    let allLines = lines contents
+    loop (-1) $ runTotal [] [] allLines
+
+loop :: Int -> [String] -> IO ()
+loop _ [] = return ()
+loop numPrevLines (status:xs) = do
+    cursorUpLine $ numPrevLines
+    showStatus status
+    loop (length $ lines status) xs
+
+showStatus :: String -> IO ()
+showStatus status = do
+    mapM_ showCurrentStatus $ lines status
+        where
+        showCurrentStatus line = do
+            clearFromCursorToLineEnd
+            putStrLn line
+
+runTotal :: [String] -> [Integer] -> [String] -> [String]
+runTotal _ _ [] = []
+runTotal seen counts (x:xs) = status : (runTotal seen' counts' xs)
+    where
+    status = unlines $ map concat $ map (intersperse "\t") [[word, show count] | (word, count) <- zip seen' counts']
+    seen'
+        | x `elem` seen = seen
+        | otherwise = x : seen
+    counts'
+        | x `elem` seen = updateIndex (fromJust $ elemIndex x seen) counts (counts !! (fromJust $ elemIndex x seen) + 1)
+        | otherwise = 1 : counts
+
+updateIndex :: Int -> [a] -> a -> [a]
+updateIndex k xs newx = take k xs ++ [newx] ++ drop (k + 1) xs
