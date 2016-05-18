5b60fc55ccba21a7680cad28683fe9d8530f79b1
Wesley Baugh <wesley@bwbaugh.com>
2016-05-18 00:12:41 -0700

    Write in another thread to throttle output

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -14,6 +14,7 @@ Added
 =====
 
 * Option to display percentage for each line.
+* Throttle output by updating once per second.
 
 
 *******************
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Concurrent
 import Control.Monad
 import Data.List (sortBy)
 import Data.Ord (comparing)
@@ -14,31 +15,34 @@ main = do
     args <- getArgs
     let showPercent = any (`elem` args) ["-p", "--percent"]
     allLines <- fmap lines getContents
-    loop (-1) $ runTotal HM.empty showPercent allLines
+    seenMVar <- newMVar HM.empty
+    _ <- forkIO (displayOutput (-1) seenMVar showPercent)
+    forM_ allLines $ \x -> do
+        seen <- takeMVar seenMVar
+        putMVar seenMVar (HM.insertWith (+) x 1 seen)
 
-loop :: Int -> [String] -> IO ()
-loop _ [] = return ()
-loop numPrevLines (status:xs) = do
+displayOutput :: Int -> MVar (HM.HashMap String Integer) -> Bool -> IO ()
+displayOutput numPrevLines seenMVar showPercent = forever $ do
+    seen <- readMVar seenMVar
     cursorUpLine numPrevLines
+    let status = makeStatus seen showPercent
     showStatus status
-    loop (length $ lines status) xs
+    threadDelay 1000000
+    displayOutput (length $ lines status) seenMVar showPercent
 
 showStatus :: String -> IO ()
 showStatus status = forM_ (lines status) $ \line -> do
     clearFromCursorToLineEnd
     putStrLn line
 
-runTotal :: HM.HashMap String Integer -> Bool -> [String] -> [String]
-runTotal _ _ [] = []
-runTotal seen showPercent (x:xs) = status : runTotal seen' showPercent xs
-    where
-    status = unlines $ map unwords sortedValues
+makeStatus :: HM.HashMap String Integer -> Bool -> String
+makeStatus seen showPercent = unlines $ map unwords sortedValues
+  where
     sortedValues = sortBy (comparing last) countValuePairs
     -- TODO(bwbaugh|2015-11-13): Pull out all formatting to another function.
     countValuePairs
-        | showPercent = [[printf "%4d" count, percent count, word] | (word, count) <- HM.toList seen']
-        | otherwise = [[printf "%4d" count, word] | (word, count) <- HM.toList seen']
-        where
+        | showPercent = [[printf "%4d" count, percent count, word] | (word, count) <- HM.toList seen]
+        | otherwise = [[printf "%4d" count, word] | (word, count) <- HM.toList seen]
+      where
         percent count = printf "(%.2f%%)" ((fromIntegral count :: Float) / total * 100)
         total = fromIntegral $ sum (HM.elems seen)
-    seen' = HM.insertWith (+) x 1 seen
