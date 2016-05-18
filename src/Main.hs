9abe0599cc1b59c454ade00c1f70ac15a90cfa1b
Wesley Baugh <wesley@bwbaugh.com>
2016-05-17 23:09:26 -0700

    Switch to HashMap from two lists

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,20 @@
 module Main where
 
 import Control.Monad
-import Data.List (elemIndex, sortBy)
+import Data.List (sortBy)
 import Data.Ord (comparing)
 import System.Environment (getArgs)
 import Text.Printf (printf)
 
 import System.Console.ANSI (clearFromCursorToLineEnd, cursorUpLine)
+import qualified Data.HashMap.Strict as HM
 
 main :: IO ()
 main = do
     args <- getArgs
     let showPercent = any (`elem` args) ["-p", "--percent"]
     allLines <- fmap lines getContents
-    loop (-1) $ runTotal [] [] showPercent allLines
+    loop (-1) $ runTotal HM.empty showPercent allLines
 
 loop :: Int -> [String] -> IO ()
 loop _ [] = return ()
@@ -27,26 +28,17 @@ showStatus status = forM_ (lines status) $ \line -> do
     clearFromCursorToLineEnd
     putStrLn line
 
-runTotal :: [String] -> [Integer] -> Bool -> [String] -> [String]
-runTotal _ _ _ [] = []
-runTotal seen counts showPercent (x:xs) = status : runTotal seen' counts' showPercent xs
+runTotal :: HM.HashMap String Integer -> Bool -> [String] -> [String]
+runTotal _ _ [] = []
+runTotal seen showPercent (x:xs) = status : runTotal seen' showPercent xs
     where
     status = unlines $ map unwords sortedValues
     sortedValues = sortBy (comparing last) countValuePairs
     -- TODO(bwbaugh|2015-11-13): Pull out all formatting to another function.
     countValuePairs
-        | showPercent = [[printf "%4d" count, percent count, word] | (count, word) <- zip counts' seen']
-        | otherwise = [[printf "%4d" count, word] | (count, word) <- zip counts' seen']
+        | showPercent = [[printf "%4d" count, percent count, word] | (word, count) <- HM.toList seen']
+        | otherwise = [[printf "%4d" count, word] | (word, count) <- HM.toList seen']
         where
         percent count = printf "(%.2f%%)" ((fromIntegral count :: Float) / total * 100)
-        total = fromIntegral $ sum counts'
-    xIndexMaybe = elemIndex x seen
-    seen' = case xIndexMaybe of
-        Just _ -> seen
-        Nothing -> x : seen
-    counts' = case xIndexMaybe of
-        Just xIndex -> updateIndex xIndex counts (counts !! xIndex + 1)
-        Nothing -> 1 : counts
-
-updateIndex :: Int -> [a] -> a -> [a]
-updateIndex k xs newx = take k xs ++ [newx] ++ drop (k + 1) xs
+        total = fromIntegral $ sum (HM.elems seen)
+    seen' = HM.insertWith (+) x 1 seen
